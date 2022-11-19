334cc1498fc832e2ee120c3418c7097f89b8f991
Zachariah Delft <zdelft@sfu.ca>
2022-11-18 23:05:45 -0800

    basic setup of gps portion currently as a fragment

--- app/src/main/java/com/example/projectlfg/MainActivity.kt
+++ app/src/main/java/com/example/projectlfg/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.projectlfg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.example.projectlfg.event_map.MapsFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    // Fragments to be put into an array
+    private lateinit var mapsFragment: MapsFragment
+    // fragment array
+    private lateinit var fragments: ArrayList<Fragment>
+    // tab layout and view pager setup
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager2: ViewPager2
+    // Fragment state adapter
+    private lateinit var myFragmentStateAdapter: MyFragmentStateAdapter
+    // Items for tab creation
+    private var tabLabels = arrayOf("Event Map")
+    // Tab config strategy and layout mediator
+    private lateinit var tabConfigurationStrategy: TabLayoutMediator.TabConfigurationStrategy
+    private lateinit var tabLayoutMediator: TabLayoutMediator
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // temp way to display map fragment
+        mapsFragment = MapsFragment()
+        fragments = ArrayList()
+        fragments.add(mapsFragment)
+        tabLayout = findViewById(R.id.tab_layout)
+        viewPager2 = findViewById(R.id.view_pager_2)
+        myFragmentStateAdapter = MyFragmentStateAdapter(this, fragments)
+        viewPager2.adapter = myFragmentStateAdapter
+        tabConfigurationStrategy =
+            TabLayoutMediator.TabConfigurationStrategy { tab: TabLayout.Tab, position: Int ->
+                tab.text = tabLabels[position]
+            }
+        tabLayoutMediator = TabLayoutMediator(tabLayout, viewPager2, tabConfigurationStrategy)
+        tabLayoutMediator.attach()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tabLayoutMediator.detach()
     }
 }
