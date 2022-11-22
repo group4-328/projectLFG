ea673ca74466293ea4870ca69bf5809f4cf1d931
Allen-Wei-SFU <cwa188@sfu.ca>
2022-11-21 23:53:17 -0800

    Allen - Design Login view

--- app/src/main/java/com/example/projectlfg/LogInActivity.kt
+++ app/src/main/java/com/example/projectlfg/LogInActivity.kt
@@ -2,10 +2,25 @@ package com.example.projectlfg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class LogInActivity : AppCompatActivity() {
+    private lateinit var view: View;
+    private lateinit var emailTextEdit:EditText;
+    private lateinit var passwordTextEdit:EditText;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+        view = layoutInflater.inflate(R.layout.activity_log_in,null);
+        emailTextEdit = view.findViewById(R.id.emailtextview)
+        passwordTextEdit = view.findViewById(R.id.passwordtextview);
+
+        emailTextEdit.setOnClickListener {
+
+        }
+        passwordTextEdit.setOnClickListener {
+
+        }
     }
 }
