e2df9eb7646e2c454f62778935bf835dd5be1b92
Ed0207 <38611159+Ed0207@users.noreply.github.com>
2022-11-25 20:52:09 -0800

    Main Menu finish
    setting up proper log in menu, logo

--- app/src/main/java/com/example/projectlfg/MainActivity.kt
+++ app/src/main/java/com/example/projectlfg/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.projectlfg
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
 import android.text.TextUtils
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -15,21 +18,22 @@ import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var view: View;
-    private lateinit var emailTextEdit: EditText;
-    private lateinit var passwordTextEdit: EditText;
+    private lateinit var emailTextEdit: EditText
+    private lateinit var passwordTextEdit: EditText
+    private lateinit var nameLayout: View
+    private lateinit var logoView: ImageView
 
-    private lateinit var loginButton: Button;
-    private lateinit var registerButton: Button;
+    private lateinit var loginButton: Button
+    private lateinit var registerButton: Button
 
     //authentication
     private lateinit var authenticator: FirebaseAuth
 
     //database
-    private lateinit var database: FirebaseDatabase;
+    private lateinit var database: FirebaseDatabase
 
     //ref
-    private lateinit var myref : DatabaseReference;
+    private lateinit var myref : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,25 +44,38 @@ class MainActivity : AppCompatActivity() {
         passwordTextEdit = findViewById(R.id.passwordtextview);
         loginButton = findViewById(R.id.LoginButton)
         registerButton = findViewById(R.id.RegisterButton)
+        nameLayout = findViewById(R.id.layout0)
 
+        // firebase init
         authenticator = FirebaseAuth.getInstance()
-
         database  = Firebase.database;
         myref = database.reference;
 
+        // UI init
+        logoView = findViewById(R.id.mainMenuLogo)
+        logoView.setImageResource(R.drawable.logo)
+        nameLayout.visibility = View.GONE
+
 
         loginButton.setOnClickListener {
-            if(TextUtils.isEmpty(emailTextEdit.text.toString()) || TextUtils.isEmpty(passwordTextEdit.text.toString())){
+            if(!TextUtils.isEmpty(emailTextEdit.text.toString()) && !TextUtils.isEmpty(passwordTextEdit.text.toString())){
 
             }
         }
 
         registerButton.setOnClickListener {
-            if(!TextUtils.isEmpty(emailTextEdit.text.toString()) && !TextUtils.isEmpty(passwordTextEdit.text.toString())){
-                println("signing up")
-                signUp("tmp",emailTextEdit.text.toString(),passwordTextEdit.text.toString())
+            if(nameLayout.visibility != View.GONE) {
+                if (!TextUtils.isEmpty(emailTextEdit.text.toString()) && !TextUtils.isEmpty(passwordTextEdit.text.toString()
+                    )
+                ) {
+                    println("signing up")
+                    signUp("tmp", emailTextEdit.text.toString(), passwordTextEdit.text.toString())
+                } else {
+                    Toast.makeText(this, "not enough information!", Toast.LENGTH_SHORT).show()
+                }
             }else{
-                println("put in information please")
+                nameLayout.visibility = View.VISIBLE
+                Toast.makeText(this, "Please fill out your name", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -86,12 +103,11 @@ class MainActivity : AppCompatActivity() {
                     val userinfo = UserInformation(name=name,email=email);
                     myref.child("users").child(user!!.uid).setValue(userinfo);
 
-                    println("sign up success!")
 
                 } else {
                     // If sign in fails
 
-                    println("sign up fail...")
+                    Toast.makeText(this, "sign up fail, please try again", Toast.LENGTH_SHORT).show()
                 }
             }
     }
