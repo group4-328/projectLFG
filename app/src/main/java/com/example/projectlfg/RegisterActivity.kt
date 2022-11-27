b08001cf1e3878f34fc02456654fa16f903b6def
Ed0207 <38611159+Ed0207@users.noreply.github.com>
2022-11-26 18:57:54 -0800

    small changes

--- app/src/main/java/com/example/projectlfg/RegisterActivity.kt
+++ app/src/main/java/com/example/projectlfg/RegisterActivity.kt
@@ -1,9 +1,39 @@
 package com.example.projectlfg
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class RegisterActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityRegisterBinding
+
+
+    private lateinit var NameEditText: EditText;
+    private lateinit var EmailEditText: EditText;
+    private lateinit var PasswordEditText: EditText;
+    private lateinit var RegisterButton: Button;
+    private lateinit var UserImageView : ImageView;
+
+    private lateinit var authenticator: FirebaseAuth;
+    private lateinit var myref : DatabaseReference;
+    private lateinit var storage: FirebaseStorage;
+    private lateinit var storageRef : StorageReference
+
+    private var imageUri: Uri?=null;
+    private lateinit var ImageGalleryIntent : ActivityResultLauncher<Intent>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Unknown file name conflict with 'activity_register' possibly due to git
@@ -24,4 +54,8 @@ class RegisterActivity : AppCompatActivity() {
                 popUp("Please fill out your name")
             }
     * */
+
+    companion object{
+        val PICK_IMAGE = 100;
+    }
 }
